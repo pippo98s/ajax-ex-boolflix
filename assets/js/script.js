26358eb79296a1533759fca585b0c6ba76eaba8d
pippo98s <filippo5scarcella@gmail.com>
2019-11-26 19:25:56 +0100

    milestone 3 step 3 (da unire in una funzione le due chiamate ajax)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready( function (){
   
   $("button").click( function(){
+    // chiamata per i film
     $.ajax({
       url: "https://api.themoviedb.org/3/search/movie",
       method : "GET",
@@ -38,7 +39,48 @@ $(document).ready( function (){
       error : function() {
         alert("404")
       }
-    })
+    });
+
+
+    // chiamata per le serie tv
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/tv",
+      method: "GET",
+      data: {
+        api_key: "ceb354f968c94e697decd423b41de216",
+        query: $("input").val(),
+        language: "it-IT",
+      },
+      success: function (data) {
+        // ripulisco l'output ad ogni click
+        $(".tv-container").html(" ");
+        console.log(data.results);
+
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+
+        for (var key in data.results) {
+          var item = data.results[key];
+          var mediaVoto = Math.floor(Math.round(item.vote_average) / 2);
+
+          var context = {
+            titolo: item.name,
+            originale: item.original_name,
+            lingua: item.original_language,
+            flag: selectFlag(item.original_language),
+            voto: mediaVoto,
+            stelle: generatorStars(mediaVoto)
+          };
+
+          var html = template(context);
+          $(".tv-container").append(html);
+        };
+
+      },
+      error: function () {
+        alert("404")
+      }
+    });
   })
 
 });
@@ -63,7 +105,7 @@ function selectFlag(lang){
     return "assets/img/it.png"
   } else if (lang == "us") {
     return "assets/img/us.png"
-  } else if (lang == "jp") {
+  } else if (lang == "ja") {
     return "assets/img/jp.png"
   } else if (lang == "cn") {
     return "assets/img/cn.png"
