680a45b67e88758e0fd13fab3b4480ceec813850
pippo98s <filippo5scarcella@gmail.com>
2019-11-28 18:38:05 +0100

    chiamate ajax unite in una funzione e aggiunto film e serie tv popolari

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,93 +1,97 @@
 $(document).ready( function (){
   
+  test();
+
   $("button").click( function(){
-    // chiamata per i film
-    $.ajax({
-      url: "https://api.themoviedb.org/3/search/movie",
-      method : "GET",
-      data : {
-        api_key: "ceb354f968c94e697decd423b41de216",
-        query: $("input").val(),
-        language : "it-IT",
-      },
-      success : function (data){
-        // ripulisco l'output ad ogni click
-        $(".film-container").html(" ");
-        
-        var source = $("#entry-template").html();
-        var template = Handlebars.compile(source);
-
-        for (var key in data.results) {
-          var item = data.results[key];
-          var mediaVoto = Math.floor(Math.round(item.vote_average) / 2);
-
-          var context = { 
-            poster: poster(item.poster_path , "w185") ,
-            titolo: item.title,
-            originale: item.original_title,
-            lingua: item.original_language,
-            flag: selectFlag(item.original_language),
-            voto: mediaVoto,
-            stelle: generatorStars(mediaVoto)
-          };
-          
-          var html = template(context);
-          $(".film-container").append(html);
-        };
-        
-        // if (data.results.length == 0){
-        //   $("h2").text("0 risultati per : " + $("input").val())
-        // }
-      },
-      error : function() {
-        alert("404")
-      }
-    });
-
-
-    // chiamata per le serie tv
-    $.ajax({
-      url: "https://api.themoviedb.org/3/search/tv",
-      method: "GET",
-      data: {
-        api_key: "ceb354f968c94e697decd423b41de216",
-        query: $("input").val(),
-        language: "it-IT",
-      },
-      success: function (data) {
-        // ripulisco l'output ad ogni click
-        $(".tv-container").html(" ");
-        console.log(data.results);
-
-        var source = $("#entry-template").html();
-        var template = Handlebars.compile(source);
-
-        for (var key in data.results) {
-          var item = data.results[key];
-          var mediaVoto = Math.floor(Math.round(item.vote_average) / 2);
-
-          var context = {
-            poster: poster(item.poster_path, "w185"),
-            titolo: item.name,
-            originale: item.original_name,
-            lingua: item.original_language,
-            flag: selectFlag(item.original_language),
-            voto: mediaVoto,
-            stelle: generatorStars(mediaVoto)
-          };
-
-          var html = template(context);
-          $(".tv-container").append(html);
-        };
-
-      },
-      error: function () {
-        alert("404")
-      }
-    });
+    search()
   })
 
 });
+
+
+// blocco funzioni
+function test() {
+  var urlTopMovie = "https://api.themoviedb.org/3/movie/popular";
+  var urlTopTv = "https://api.themoviedb.org/3/tv/popular";
+  var q = $("input").val();
+  getDataTest(urlTopMovie, 'movieTop');
+  getDataTest(urlTopTv, q, 'tvTop');
+}
+
+function search() {
+  var urlMovie = 'https://api.themoviedb.org/3/search/movie';
+  var urlTv = 'https://api.themoviedb.org/3/search/tv';
+  var q = $("input").val();
+  getData(urlMovie, q, 'movie');
+  getData(urlTv, q, 'tv');
+}
+
+function getData(url, query, type) {
+  var apiKey = "ceb354f968c94e697decd423b41de216";
+  $.ajax({
+    url: url,
+    method: "GET",
+    data: {
+      api_key: apiKey,
+      query: query,
+      language: "it-IT"
+    },
+    success: function (data) {
+      var elements = data.results;
+      print(type, elements);
+    },
+    error: function (err) {
+      console.log(err);
+    }
+  });
+}
+
+function getDataTest(url, type) {
+  var apiKey = "ceb354f968c94e697decd423b41de216";
+  $.ajax({
+    url: url,
+    method: "GET",
+    data: {
+      api_key: apiKey,
+      language: "it-IT"
+    },
+    success: function (data) {
+      var elements = data.results;
+      print(type, elements);
+    },
+    error: function (err) {
+      console.log(err);
+    }
+  });
+}
+
+function print(type, elems) {
+  var target = (type == "movie" || type == 'movieTop' ? $(".film-container") : $(".tv-container"));
+  target.html(" ");
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+
+  for (var key in elems) {
+    var item = elems[key];
+    var title = (type == "movie" || type == 'movieTop' ? item.title : item.name);
+    var originalTitle = (type == "movie" || type == 'movieTop' ? item.original_title : item.original_name);
+    var mediaVoto = Math.floor(Math.round(item.vote_average) / 2);
+
+    var context = {
+      poster: poster(item.poster_path, "w185"),
+      titolo: title,
+      originale: originalTitle,
+      lingua: item.original_language,
+      flag: selectFlag(item.original_language),
+      voto: mediaVoto,
+      stelle: generatorStars(mediaVoto)
+    };
+
+    var html = template(context);
+    target.append(html);
+  };
+}
+
 
 
 
@@ -107,12 +111,14 @@ function generatorStars(num){
 function selectFlag(lang){
   var array = ["en", "it", "us", "ja", "fr"]
   var i = 0;
+  var genericFlag = "assets/img/worldwide.png"
   while (i < array.length) {
     if (lang == array[i]) {
       return "assets/img/" + array[i] + ".png"
     }
     i++;
   }
+  return genericFlag
 }
 
 // funzione per mostare la copertina
